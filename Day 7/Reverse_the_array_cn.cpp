f5a41e69f760cf6181b23b696d35ff7174d48bfc
idevRavn <idevravn@gmail.com>
2024-07-11 14:23:36 +0530

    day 7

---
+++ Day 7/Reverse_the_array_cn.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int start = 0;
+    int end = n - 1;
+    while(start < end){
+        int temp = arr[start];
+        arr[start++] = arr[end];
+        arr[end--] = temp;
+    }
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
